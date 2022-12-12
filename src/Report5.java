bf80d0b0fca5b57eeb76ad39c2f2134eb61bce9b
Madoka Keye <e225731@ie.u-ryukyu.ac.jp>
2022-12-12 21:24:55 +0900

    3rd commit

--- src/Report5.java
+++ src/Report5.java
@@ -1,11 +1,7 @@
 public class Report5{
     public static void main(String[] args){
-        String str = null;
-        try{System.out.println(str.length());}
-        catch (NullPointerException e) {
-            System.out.println("NullPointerExceptionが発生している");
-            System.out.println(e.getMessage());
-            //NullPointerExceptionが起きたときの処理
-        }
+        String str = "壱百満";       
+        int val = Integer.parseInt(str);
+        System.out.println(val);
     }
 }
